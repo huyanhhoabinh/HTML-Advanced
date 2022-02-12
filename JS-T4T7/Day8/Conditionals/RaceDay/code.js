82ccb572dcb5eb9f3be613c9ca764d7ab72d6727
itsacademyinfo <Its@123456a@>
2022-02-12 19:04:07 +0700

    test

---
+++ JS-T4T7/Day8/Conditionals/RaceDay/code.js
@@ -0,0 +1,14 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = false;
+const age = 19;
+if (age > 18 && registeredEarly) {
+    raceNumber += 1000;
+}
+
+if (age > 18 && registeredEarly) {
+    console.log(`Your race will start at 9:30 am and your race number is ${raceNumber}`);
+} else if (age > 18 && !registeredEarly) {
+    console.log(`Your race will start at 11:00am am and your race number is ${raceNumber}`);
+} else if (age <= 18) {
+    console.log(`Your race will start at 12:30 pm and your race number is ${raceNumber}`);
+}
