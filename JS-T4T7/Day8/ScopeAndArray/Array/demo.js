e9e253dce00cec0d3973db96dd01af7209f0dbd7
itsacademyinfo <Its@123456a@>
2022-03-19 19:38:20 +0700

    test

--- JS-T4T7/Day8/ScopeAndArray/Array/demo.js
+++ JS-T4T7/Day8/ScopeAndArray/Array/demo.js
@@ -12,3 +12,8 @@ console.log(utensils);
 const chores = ['wash dishes', 'do laundry', 'take out trash'];
 chores.push("test", "test2");
 console.log(chores);
+// pop
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+console.log(chores);
