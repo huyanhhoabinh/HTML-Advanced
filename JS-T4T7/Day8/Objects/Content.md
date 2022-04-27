fc9683d2d410c7a5a1c576b96866c1d4c8801390
itsacademyinfo <Its@123456a@>
2022-04-27 18:45:29 +0700

    test

--- JS-T4T7/Day8/Objects/Content.md
+++ JS-T4T7/Day8/Objects/Content.md
@@ -97,4 +97,36 @@ const spaceship = {
 
 spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
 ```
-- Pass By Reference: 
+- Pass By Reference: when we pass a variable assigned to an object into a function as an argument, the computer interprets the parameter name as pointing to the space in memory holding that object. As a result, functions which change object properties actually mutate the object permanently (even when the object is assigned to a `const` variable).
+```html
+const spaceship = {
+  homePlanet : 'Earth',
+  color : 'silver'
+};
+ 
+let paintIt = obj => {
+  obj.color = 'glorious gold'
+};
+ 
+paintIt(spaceship);
+ 
+spaceship.color // Returns 'glorious gold'
+ 
+```
+
+- Looping Through Objects
+```html
+
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+  console.log(`${property}: ${object[property]}`);
+}
+
+// expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"
+
+
+```
--- JS-T4T7/Day8/Objects/demo.js
+++ JS-T4T7/Day8/Objects/demo.js
@@ -98,3 +98,146 @@ let firstPassenger = spaceship.passengers[0];
 
 
 
+/*Pass by reference*/
+
+const spaceship = {
+    homePlanet : 'Earth',
+    color : 'silver'
+};
+
+let paintIt = obj => {
+    obj.color = 'glorious gold'
+};
+
+paintIt(spaceship);
+
+spaceship.color // Returns 'glorious gold'
+
+//
+let spaceship = {
+    homePlanet : 'Earth',
+    color : 'red'
+};
+let tryReassignment = obj => {
+    obj = {
+        identified : false,
+        'transport type' : 'flying'
+    }
+    console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+
+};
+tryReassignment(spaceship) // The attempt at reassignment does not work.
+spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
+
+spaceship = {
+    identified : false,
+    'transport type': 'flying'
+}; // Regular reassignment still works.
+
+/* However, reassignment of the spaceship variable wouldn’t work in the same way:*/
+
+let spaceship = {
+    homePlanet : 'Earth',
+    color : 'red'
+};
+let tryReassignment = obj => {
+    obj = {
+        identified : false,
+        'transport type' : 'flying'
+    }
+    console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+
+};
+tryReassignment(spaceship) // The attempt at reassignment does not work.
+spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
+
+spaceship = {
+    identified : false,
+    'transport type': 'flying'
+}; // Regular reassignment still works.
+
+/*Demo*/
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+};
+
+// Write your code below
+let greenEnergy = obj => {
+    obj['Fuel Type'] = 'avocado oil';
+};
+
+let remotelyDisable = obj => {
+    obj.disabled = true;
+};
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+console.log(spaceship);
+
+/*For in*/
+
+let spaceship = {
+    crew: {
+        captain: {
+            name: 'Lily',
+            degree: 'Computer Engineering',
+            cheerTeam() { console.log('You got this!') }
+        },
+        'chief officer': {
+            name: 'Dan',
+            degree: 'Aerospace Engineering',
+            agree() { console.log('I agree, captain!') }
+        },
+        medic: {
+            name: 'Clementine',
+            degree: 'Physics',
+            announce() { console.log(`Jets on!`) } },
+        translator: {
+            name: 'Shauna',
+            degree: 'Conservation Science',
+            powerFuel() { console.log('The tank is full!') }
+        }
+    }
+};
+
+// for...in
+for (let crewMember in spaceship.crew) {
+    console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+}
+
+let spaceship = {
+    crew: {
+        captain: {
+            name: 'Lily',
+            degree: 'Computer Engineering',
+            cheerTeam() { console.log('You got this!') }
+        },
+        'chief officer': {
+            name: 'Dan',
+            degree: 'Aerospace Engineering',
+            agree() { console.log('I agree, captain!') }
+        },
+        medic: {
+            name: 'Clementine',
+            degree: 'Physics',
+            announce() { console.log(`Jets on!`) } },
+        translator: {
+            name: 'Shauna',
+            degree: 'Conservation Science',
+            powerFuel() { console.log('The tank is full!') }
+        }
+    }
+};
+
+// Write your code below
+for(let crew in spaceship.crew) {
+    console.log(`${crew}: ${spaceship.crew[crew].name}`);
+}
+
+
+for(let crew in spaceship.crew) {
+    console.log(`${spaceship.crew[crew].name}: ${spaceship.crew[crew].degree}`);
+}
