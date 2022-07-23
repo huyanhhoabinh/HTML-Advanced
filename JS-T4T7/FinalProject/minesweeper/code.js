7d7c79bf9f04af901f9ee6f9496b3a7c087262f5
itsacademyinfo <Its@123456a@>
2022-07-23 16:53:37 +0700

    test

--- JS-T4T7/FinalProject/minesweeper/code.js
+++ JS-T4T7/FinalProject/minesweeper/code.js
@@ -4,11 +4,23 @@ var columns = 8;
 var minesCount = 5;
 var minesLocation = [];
 var titlesClicked = 0;
-var flagEnabled = false;
 var gameOver = false;
 window.onload = function () {
     window.addEventListener("contextmenu", e => e.preventDefault());
     startGame();
+}
+function reset() {
+    let x = document.getElementById("board");
+    x.innerHTML = "";
+    board = [];
+    rows = 8;
+    columns = 8;
+    minesCount = 5;
+    minesLocation = [];
+    titlesClicked = 0;
+    gameOver = false;
+    startGame();
+
 }
 function setMines() {
     let mineLeft = minesCount;
@@ -23,31 +35,41 @@ function setMines() {
     }
 }
 function startGame() {
-    // document.getElementsByName("body")[0].addEventListener("click", function (e){
-    //     e.preventDefault();
-    //     e.stopPropagation();
-    // });
     document.getElementById("mines-count").innerText = minesCount;
-    document.getElementById("flag-button").addEventListener("click", setFlag);
     setMines();
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-            tile.addEventListener("click", clickTile);
+            tile.addEventListener("mousedown", clickTile);
             document.getElementById("board").append(tile);
             row.push(tile);
         }
         board.push(row);
     }
 }
-function clickTile() {
-    if(gameOver || this.classList.contains("tile-clicked")) {
-        return;
-    }
+function clickTile(e) {
     let tile = this;
-    if(flagEnabled) {
+    if(e.button === 0) {
+        if(tile.innerText == "🚩") {
+            return;
+        }
+        if(gameOver || this.classList.contains("tile-clicked")) {
+            return;
+        }
+        if(minesLocation.includes(tile.id)) {
+            alert("Game Over");
+            gameOver = true;
+            revealMines();
+            return;
+        }
+        let coords = tile.id.split("-");
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+        checkMines(r,c);
+    }
+    if(e.button === 2) {
         if(tile.innerText == "") {
             tile.innerText = "🚩";
         }
@@ -56,16 +78,6 @@ function clickTile() {
         }
         return;
     }
-    if(minesLocation.includes(tile.id)) {
-      alert("Game Over");
-      gameOver = true;
-      revealMines();
-      return;
-    }
-    let coords = tile.id.split("-");
-    let r = parseInt(coords[0]);
-    let c = parseInt(coords[1]);
-    checkMines(r,c);
 }
 function checkMines(r, c) {
     if(r < 0 || r >= rows || c < 0 || c >= columns) {
@@ -125,13 +137,13 @@ function revealMines() {
         }
     }
 }
-function setFlag() {
-    if(flagEnabled) {
-        flagEnabled = false;
-        document.getElementById("flag-button").style.backgroundColor = "lightgray";
-    } else {
-        flagEnabled = true;
-        document.getElementById("flag-button").style.backgroundColor = "darkgray";
-
-    }
-}
+// function setFlag() {
+//     if(flagEnabled) {
+//         flagEnabled = false;
+//         document.getElementById("flag-button").style.backgroundColor = "lightgray";
+//     } else {
+//         flagEnabled = true;
+//         document.getElementById("flag-button").style.backgroundColor = "darkgray";
+//
+//     }
+// }
